fe29b44cac654d3ddd08cb37db65e1f3a18c394e
TianRui-Song717 <1943651008@qq.com>
2025-11-11 18:29:56 +0000

    Deploying to gh-pages from @ TianRui-Song717/TianRui-Song717.github.io@cb29cecee564113e10cbbb7fab1031bb6fc2fe2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -387,7 +387,7 @@ ninja.data = [{
             },},{id: "news-our-paper-mm-agent-llms-as-agents-for-real-world-mathematical-modeling-problems-has-been-accepted-to-the-neurips-2025",
           title: 'Our paper “MM-Agent: LLMs as Agents for Real-world Mathematical Modeling Problems” has been...',
           description: "",
-          section: "News",},{id: "news-our-paper-hard-vs-noise-resolving-hard-noisy-sample-confusion-in-recommender-systems-via-large-language-models-is-accepted-by-aaai2026",
+          section: "News",},{id: "news-our-paper-hard-vs-noise-resolving-hard-noisy-sample-confusion-in-recommender-systems-via-large-language-models-is-accepted-by-aaai-2026",
           title: 'Our paper “Hard vs. Noise: Resolving Hard-Noisy Sample Confusion in Recommender Systems via...',
           description: "",
           section: "News",},{id: "projects-project-1",
